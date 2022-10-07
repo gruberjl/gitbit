f99ef5ca8f263d237e832c1274f7ff82af939d55
John Gruber <gruberjl@gmail.com>
2022-10-07 17:33:25 -0400

    fixed adsense

--- src/components/page.js
+++ src/components/page.js
@@ -1,13 +1,20 @@
 import {h} from 'preact'
+import {useEffect} from 'preact/hooks'
 import Heading from './heading'
 import PageHeader from './page-header'
 
 export default function Page({children, canonical, title, description, jsonLdType, image, jsonLd}) {
+  useEffect(() => {
+    if (window)
+      (window.adsbygoogle = window.adsbygoogle || []).push({})
+  }, [])
+
   return (
     <div>
       <Heading canonical={canonical} title={title} description={description} jsonLdType={jsonLdType} image={image} jsonLd={jsonLd} />
       <PageHeader />
       {children}
+      <ins class="adsbygoogle" data-ad-client="ca-pub-8622067882965868" />
     </div>
   )
 }
